c2fb0beed966b00dd70877b5f4842a5763b38a4a
Alex Ortega <alexortega@alexs-mbp.lan>
2020-06-15 10:25:18 -0400

    added styling to make pages fill the browser window

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,7 @@ User.destroy_all
 @user = User.create!(username: 'Alex', email: 'alex@gmail.com', password: '123456')
 @team1 = Team.create!(name: 'Team 1', user: @user, sport_type: 'basketball')
 @team2 = Team.create!(name: 'Team 2', user: @user, sport_type: 'soccer')
+@team3 = Team.create!(name: 'Team 3', user: @user, sport_type: 'basketball')
 
 @victoria = Player.create!(user: @user, team: @team1, name: 'Victoria', height: 66, sex: 'Female', jersey: 4)
 @alex = Player.create!(user: @user, team: @team1, name: 'Alex', height: 73, sex: 'Male', jersey: 28)
@@ -35,6 +36,15 @@ User.destroy_all
 @fred = Player.create!(user: @user, team: @team2, name: 'Fred', height: 73, sex: 'Male', jersey: 18)
 @lily = Player.create!(user: @user, team: @team2, name: 'Lily', height: 71, sex: 'Male', jersey: 19)
 @alec = Player.create!(user: @user, team: @team2, name: 'Alec', height: 72, sex: 'Male', jersey: 20)
+
+@jess = Player.create!(user: @user, team: @team3, name: 'Jess', height: 66, sex: 'Female', jersey: 4)
+@chris = Player.create!(user: @user, team: @team3, name: 'Chris', height: 73, sex: 'Male', jersey: 28)
+@nick = Player.create!(user: @user, team: @team3, name: 'Nick', height: 71, sex: 'Male', jersey: 9)
+@drew = Player.create!(user: @user, team: @team3, name: 'Drew', height: 72, sex: 'Male', jersey: 2)
+@sarah = Player.create!(user: @user, team: @team3, name: 'Sarah', height: 66, sex: 'Female', jersey: 5)
+@tony = Player.create!(user: @user, team: @team3, name: 'Tony', height: 73, sex: 'Male', jersey: 29)
+@lyle = Player.create!(user: @user, team: @team3, name: 'Lyle', height: 71, sex: 'Male', jersey: 10)
+@dave = Player.create!(user: @user, team: @team3, name: 'Dave', height: 72, sex: 'Male', jersey: 2)
 
 # Team.create!(name: 'Team 1', user: @user, players: [@victoria, @alex, @raf, @andrew])
 
